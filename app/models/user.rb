a752409286b8cc1bbd4dc342b32df0dfef85573b
Alex Moreno <alejandro.moreno.salas@gmail.com>
2013-11-16 01:46:12 +0000

    cambios para poder guardar un usuario, before_validation : encrypt_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :password
-  before_save :encrypt_password
+  before_validation :encrypt_password
   has_many :uploads
   has_many :rates
   has_many :comments
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
   end
     
   def encrypt_password
-    if password.present?
+    if self.password.present?
       self.password_salt = BCrypt::Engine.generate_salt 
       self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
     end
